86af7cbbe596fe14aac2977861ae4ecc25730cc1
samu.sar <samuil.sarandev@gmail.com>
2024-03-15 12:14:15 +0200

    Add: added link button component

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,5 @@
+<a
+    href='{{ $href }}'
+    class='rounded-md border border-slate-300 bg-white px-2.5 py-1.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-slate-100 transition'>
+    {{ $slot }}
+</a>
