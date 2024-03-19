25031414795ab88c513b523b072a0d4aef18188a
samu.sar <samuil.sarandev@gmail.com>
2024-03-19 11:55:00 +0200

    Add: implemented BE search by title or description

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,19 +2,23 @@
     <x-breadcrumbs class='mb-4' :links="['Jobs' => route('jobs.index')]" />
 
     <x-card class='mb-4 text-sm'>
-        <div class='mb-4 grid grid-cols-2 gap-4'>
-            <div>
-                <div class="mb-1 font-semibold">Search</div>
-                <x-text-input name='search' value='' placeholder='Search for any text'></x-text-input>
-            </div>
-            <div>
-                <div class="mb-1 font-semibold">Salary</div>
-                <div class="flex space-x-2">
-                    <x-text-input name='min_salary' value='' placeholder='From'></x-text-input>
-                    <x-text-input name='max_salary' value='' placeholder='To'></x-text-input>
+        <form action='{{ route('jobs.index') }}' method='GET'>
+            <div class='mb-4 grid grid-cols-2 gap-4'>
+                <div>
+                    <div class="mb-1 font-semibold">Search</div>
+                    <x-text-input name='search' value='' placeholder='Search for any text'></x-text-input>
                 </div>
-            </div>
-        </div> 
+                <div>
+                    <div class="mb-1 font-semibold">Salary</div>
+                    <div class="flex space-x-2">
+                        <x-text-input name='min_salary' value='' placeholder='From'></x-text-input>
+                        <x-text-input name='max_salary' value='' placeholder='To'></x-text-input>
+                    </div>
+                </div>
+            </div> 
+
+            <button class='w-full'>Filter</button>
+        </form>
     </x-card>
     
     @foreach ($jobs as $job)
