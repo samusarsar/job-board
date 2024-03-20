8f217e60f695ead32ad9dc1f6e07aa8b1751cf91
samu.sar <samuil.sarandev@gmail.com>
2024-03-20 11:57:36 +0200

    Style: style background with gradient

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel Job Board</title>
         @vite('resources/css/app.css')
     </head>
-    <body class="px-5 mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+    <body class="px-5 mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 via-sky-100 to-emerald-100 text-slate-700">
         {{ $slot }}
     </body>
 </html>
