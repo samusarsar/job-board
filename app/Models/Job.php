5540bad885b1a4b3973a441cbf9986fb596a8798
samu.sar <samuil.sarandev@gmail.com>
2024-03-20 13:18:49 +0200

    Refactor: refactored job filtering with model scope

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,20 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Job extends Model
 {
     use HasFactory;
 
-    public static array $experience = [
-        'entry',
-        'intermediate',
-        'senior'
-    ];
-    public static array $category = [
-        'IT',
-        'Finance',
-        'Sales',
-        'Marketing'
-    ];
+    public static array $experience = ["entry", "intermediate", "senior"];
+    public static array $category = ["IT", "Finance", "Sales", "Marketing"];
+
+    public function scopeFilter(Builder $query, array $filters): Builder
+    {
+        return $query
+            ->when($filters["search"] ?? null, function ($query, $search) {
+                $query->where(function ($query) use ($search) {
+                    $query
+                        ->where("title", "like", "%" . $search . "%")
+                        ->orWhere("description", "like", "%" . $search . "%");
+                });
+            })
+            ->when($filters["min_salary"] ?? null, function (
+                $query,
+                $minSalary
+            ) {
+                $query->where("salary", ">=", $minSalary);
+            })
+            ->when($filters["max_salary"] ?? null, function (
+                $query,
+                $maxSalary
+            ) {
+                $query->where("salary", "<=", $maxSalary);
+            })
+            ->when($filters["experience"] ?? null, function (
+                $query,
+                $experience
+            ) {
+                $query->where("experience", $experience);
+            })
+            ->when($filters["category"] ?? null, function ($query, $category) {
+                $query->where("category", $category);
+            });
+    }
 }
