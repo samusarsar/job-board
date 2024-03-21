2e66433e81dafe10540570ec942c65ed761443bd
samu.sar <samuil.sarandev@gmail.com>
2024-03-21 13:42:04 +0200

    Add: added JobApplicationController with initial create route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,5 +22,13 @@ Route::resource("jobs", JobController::class)->only(["index", "show"]);
 
 Route::get("login", fn() => to_route("auth.create"))->name("login");
 Route::resource("auth", AuthController::class)->only(["create", "store"]);
+
 Route::delete("logout", fn() => to_route("auth.destroy"))->name("logout");
 Route::delete("auth", [AuthController::class, "destroy"])->name("auth.destroy");
+
+Route::middleware("auth")->group(function () {
+    Route::resource("jobs.application", JobApplicationController::class)->only([
+        "create",
+        "store",
+    ]);
+});
