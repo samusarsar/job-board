ae953ed8bdf3808052afb782d7005929247b3d3f
samu.sar <samuil.sarandev@gmail.com>
2024-03-21 12:00:41 +0200

    Add: added base auth navigation to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,30 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="px-5 mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 via-sky-100 to-emerald-100 text-slate-700">
+        <nav class="mb-8 flex justify-between text-large font-medium">
+            <ul class="flex space-x-2">
+                <li>
+                    <a href="{{ route('jobs.index') }}">Home</a>
+                </li>
+            </ul>
+
+            <ul class="flex space-x-2">
+                @auth
+                    <li>{{ auth()->user()->name ?? 'Anonymous' }}</li>
+                    <li>
+                        <form action="{{ route('auth.destroy') }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <x-button>Logout</x-button>
+                        </form>
+                    </li>
+                @else
+                    <li>
+                        <a href="{{ route('auth.create') }}">Sign in</a>
+                    </li>
+                @endauth
+            </ul>
+        </nav>
         {{ $slot }}
     </body>
 </html>
