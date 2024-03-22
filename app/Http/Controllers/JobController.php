90358e855ff625d7b4578e76d161df4076c0e0bb
samu.sar <samuil.sarandev@gmail.com>
2024-03-22 11:53:32 +0200

    Add: added new job creation functionality to my_job.index

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -21,7 +21,7 @@ class JobController extends Controller
         ]);
 
         return view("job.index", [
-            "jobs" => Job::with("employer")->filter($filters)->get(),
+            "jobs" => Job::with("employer")->latest()->filter($filters)->get(),
         ]);
     }
 
